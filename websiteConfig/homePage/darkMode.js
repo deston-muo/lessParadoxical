36244e4a9572e408f416c5ffce25fa9d56528de8
deston-muo <kobimuo@gmail.com>
2022-03-05 20:01:16 -0600

    Addition Of Dark Mode

---
+++ websiteConfig/homePage/darkMode.js
@@ -0,0 +1,30 @@
+var darkModeSlider = document.getElementById('dark-mode-slider');
+var darkModeButton = document.getElementById('dark-mode-button');
+var body = document.body;
+
+
+var buttonClicked = false;
+
+darkModeButton.onclick = function() {
+    if(buttonClicked == false) {
+    darkModeButton.style.backgroundColor = "white";
+    darkModeButton.style.transition = '1s'
+    darkModeSlider.style.backgroundColor = "black";
+    darkModeSlider.style.transform = 'translate(0px, 4.5vh)';
+    darkModeSlider.style.transition = 'transform .4s ease-in-out'
+    body.style.backgroundColor = "black";
+    body.style.transition = '1s';
+    buttonClicked = true;
+
+    }else if(buttonClicked == true) {
+    darkModeButton.style.backgroundColor = "black";
+    darkModeButton.style.transition = '1s'
+    darkModeSlider.style.backgroundColor = "white";
+    darkModeSlider.style.transform = 'translate(0px, -2.2px)';
+    darkModeSlider.style.transition = 'transform .4s ease-in-out'
+    body.style.backgroundColor = "white";
+    body.style.transition = '1s';  
+
+    buttonClicked = false;  
+    }
+}
